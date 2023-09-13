df40c4fefb21a73d7a6124b3ed91479740ab2477
Alexandre Gascon <alexandregascon@sfr.fr>
2023-09-13 15:32:47 +0200

    test hook

--- sql/cs_2023_cafe.sql
+++ sql/cs_2023_cafe.sql
@@ -1,6 +1,6 @@
 -- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for Win64 (AMD64)
 --
--- Host: localhost    Database: cs_2023_cafe
+-- Host: localhost    Database: secret
 -- ------------------------------------------------------
 -- Server version	10.4.28-MariaDB
 
