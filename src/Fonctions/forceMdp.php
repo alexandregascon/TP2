f924a054ffca1c62f771c22b7c2bb5507809ba87
Alexandre Gascon <alexandregascon@sfr.fr>
2023-10-11 13:55:34 +0200

    test hook

---
+++ src/Fonctions/forceMdp.php
@@ -0,0 +1,22 @@
+<?php
+
+$nbCaracPossible = readline("Nb de caractères possibles : ");
+$nbLongueurMdp = readline("Nb longueur mdp : ");
+
+
+function floatToBinary($num)
+{
+    $binary = '';
+    $num -= 1;
+    while ($num > 0) {
+        $bit = $num % 2;
+        $binary = $bit . $binary;
+        $num = ($num - $bit) / 2;
+    }
+    return $binary;
+}
+
+$floatNumber = $nbCaracPossible ** $nbLongueurMdp;
+$binaryRepresentation = strlen(floatToBinary($floatNumber));
+
+echo "Le nombre binaire de $floatNumber est : $binaryRepresentation";
