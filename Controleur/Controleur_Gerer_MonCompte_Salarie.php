28e013397490344208a5baede43c6ffc77de1241
Alexandre Gascon <alexandregascon@sfr.fr>
2023-12-18 15:19:56 +0100

    Information Q7

--- Controleur/Controleur_Gerer_MonCompte_Salarie.php
+++ Controleur/Controleur_Gerer_MonCompte_Salarie.php
@@ -74,6 +74,29 @@ switch ($action) {
     case "RecupInfos":
         $salarie = Modele_Salarie::Salarie_Select_byId_ForJson($_SESSION["idSalarie"]);
         $json = json_encode($salarie);
+        file_put_contents("infos.json",$json);
+
+        $full_path = "infos.json"; // chemin système (local) vers le fichier
+        $file_name = basename($full_path);
+
+        ini_set('zlib.output_compression', 0);
+        $date = gmdate(DATE_RFC1123);
+
+        header('Pragma: public');
+        header('Cache-Control: must-revalidate, pre-check=0, post-check=0, max-age=0');
+
+        header('Content-Tranfer-Encoding: none');
+        header('Content-Length: '.filesize($full_path));
+        header('Content-MD5: '.base64_encode(md5_file($full_path)));
+        header('Content-Type: application/octetstream; name="'.$file_name.'"');
+        header('Content-Disposition: attachment; filename="'.$file_name.'"');
+
+        header('Date: '.$date);
+        header('Expires: '.gmdate(DATE_RFC1123, time()+1));
+        header('Last-Modified: '.gmdate(DATE_RFC1123, filemtime($full_path)));
+
+        readfile($full_path);
+        exit;
         break;
     case "AnnulerRGPD":
         \App\Modele\Modele_Utilisateur::utilisateur_desactiver_rgpd($_SESSION["idSalarie"]);
